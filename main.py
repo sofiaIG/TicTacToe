76429c1208cb313df36129501443e29684b10f4b
sofiaIG <sphignatiadi@gmail.com>
2021-08-21 20:01:29 +0100

    I have added quite a lot of functions. I have amended the enter_input function as previously I had a very long if-else statement. This stament has now been moved to a new function named adding_decision_into_board. I have added more a funtion removed squares as I want a board that only shows that remaining options (the ones that have not been selected). Also, I have added another funtion called user_input. This is to validate input and make sure that it is an integer. I have also created a funtion called opponents_move. This will select a number randomly from the romoved_squares board. We also have two functions. This will be fixed in the future as they are too long. Lastly, the loop runs, until someone wins.

--- main.py
+++ main.py
@@ -1,52 +1,150 @@
-values = [x for x in range(1,10)]
-board =[]
+import random
+#Bugs:
+#-Board once the there are no other spaces. Need message that the game is finished (this has been completed)
+#-Player needs to be able to win diagonally which they cannot now (this has been completed too).
+#-We need to control the input of the player. Currently if you enter a space that is already taken you are losing your turn
+#Also we need to do input validation. Needs to be an int that is from 0-8
 x = "X"
+o = "O"
+board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+number_count = 0
+for i in range(0, 3):
+    count = 0
+    for j in range(1, 4):
+        board[i][count] = number_count
+        if count >= 9:
+            break
+        else:
+            count += 1
+            number_count += 1
 
-for i in range(3):
-    row = [x for x in range(3)]
-    board.append(row)
 
+def display_board(board: list):
+    "This function in printing the board with its current values"
+    print("+-------+-------+-------+\n" "|       |       |       |\n"\
+          "|   {}   |   {}   |   {}   |\n"\
+          "|       |       |       |\n".format(board[0][0], board[0][1], board[0][2]), end ="")
+    print("+-------+-------+-------+\n" "|       |       |       |\n"
+          "|   {}   |   {}   |   {}   |\n"\
+          "|       |       |       |\n".format(board[1][0], board[1][1], board[1][2]), end ="")
+    print("+-------+-------+-------+\n" "|       |       |       |\n"\
+          "|   {}   |   {}   |   {}   |\n""|       |       |       |\n"\
+          "+-------+-------+-------+".format(board[2][0], board[2][1], board[2][2]), end ="")
 
-def display_board(values):
-    print("+-------+-------+-------+\n" "|       |       |       |\n""|   {}   |   {}   |   {}   |\n""|       |       |       |\n".format(values[0], values[1], values[2]), end = "")
-    print("+-------+-------+-------+\n" "|       |       |       |\n""|   {}   |   {}   |   {}   |\n""|       |       |       |\n".format(values[3], values[4], values[5]), end = "")
-    print("+-------+-------+-------+\n" "|       |       |       |\n""|   {}   |   {}   |   {}   |\n""|       |       |       |\n""+-------+-------+-------+".format(values[6], values[7], values[8]), end = "")
+def user_inpur():
+    "This function is created to check whether the user's input is an"
+    while True:
+        try:
+            users_choice = int(input("\nEnter number: "))
+        except ValueError:
+            print("Not an integer! Try again.")
+            #continue
+        else:
+            return users_choice
 
+def remove_squares_with_x(board):
+    "This is a function will create a board that only has squares that are not selected by X and O"
+    board_decision_removed = [[i for i in j if type(i)==int]for j in board]
+    return board_decision_removed
 
 def enter_input():
-    users_choice = int(input("\nPlease select a number: "))
-    if users_choice == 1:
-        board[0][0]=1
-        values[0] = x
-    elif users_choice == 2:
-        board[0][1] = 2
-        values[1] = x
-    elif users_choice ==3:
-        board[0][2] = 3
-        values[2] = x
-    elif users_choice ==4:
-        board[1][0] = 4
-        values[3] = x
-    elif users_choice == 5:
-        board[1][1] = 5
-        values[4] = x
-    elif users_choice == 6:
-        board[1][2] = 6
-        values[5] = x
-    elif users_choice == 7:
-        board[2][0] = 7
-        values[6] = x
-    elif users_choice == 8:
-        board[2][1] = 8
-        values[7] = x
-    elif users_choice == 9:
-        values[8] = x
+    "This function is used to get players decision and place it on the board"
+    board_user = remove_squares_with_x(board)# That did not work
+    users_choice = user_inpur()
+    while users_choice not in range(0,9): #and users_choice not in board_user:
+        users_choice = input("Please only enter integer from 0-8:")
+        user_inpur()
+    adding_decision_into_board(users_choice, board, x)
 
 
-display_board(values)
-enter_input()
-display_board(values)
+def opponents_move():
+    "This function is used to get the user's move and add it into the board"
+    board_without_x_o = remove_squares_with_x(board)
+    if len(board_without_x_o)==0:
+        "You have finished the game"
+    else:
+        flat = [x for sublist in board_without_x_o for x in sublist]
+        number_selected = random.choice(flat)
+        print("\nThe opponent has selected: ")
+        adding_decision_into_board(number_selected, board, o)
 
 
-def win():
-    win_combinations = [{1,2,3},{4,5,6},{7,8,9},{1,4,7},{2,5,8},{3,6,9},{1,5,9},{3,5,7}]
+def win_game(board,character):
+    "This function in used to decide who is winning. The functionality of it is limited to vertical and horizontal winning "
+    position_j = -1
+    list_vertically = []
+    list_diagonally = []
+    number = 0
+    for i in board:
+        if set(i) == {character}:
+            print(f"{character} has won!")
+            return True
+        for j in enumerate(i):
+            if j[-1] == character:
+                if position_j == -1:
+                    position_j = j[0]
+                elif position_j == j[0]:
+                    list_vertically.append(j[-1])
+    if len(list_diagonally) == 2 or len(list_vertically) == 2:
+        print(f"{character} wins")
+        return True
+    else:
+        return False
+
+def winning_diagonally(board, character):
+    number = 0
+    list_for_1st_loop = []
+    list_for_2st_loop = []
+    for i in board:
+        for j in i:
+            if i[number] == character:
+                list_for_1st_loop.append(1)
+                if number <= 2:
+                    number += 1
+                    break
+    if len(list_for_1st_loop) == 3:
+        print(f"{character} wins")
+        return True
+    else:
+        number_1 = 2
+        for i in board:
+            for j in i:
+                if i[number_1] == character:
+                    list_for_2st_loop.append(1)
+                if number_1 >= 0:
+                    number_1 -= 1
+                    break
+    if len(list_for_2st_loop) == 3:
+        print(f"{character} wins")
+        return True
+
+
+def adding_decision_into_board(value, board, character):
+    number = -1#Creating number as I can not use i for index so I am replacing it with number
+    for i in board:
+        number+=1
+        for j in i:
+            index = i.index(j)
+            if board[number][index] == value:
+                board[number][index] = character
+                break
+    return board
+
+
+
+while True:
+    display_board(board)
+    enter_input()
+    if win_game(board,x) == True or winning_diagonally(board,x) == True:
+        display_board(board)
+        break
+    flat = [i for sublist in board for i in sublist]  # This is not working
+    if set(flat) == {x, o}:
+        print("The game is finished")
+        break
+    display_board(board)
+    opponents_move()
+    if win_game(board,o) == True or winning_diagonally(board, o) == True:
+        display_board(board)
+        break
+    win_game(board, o)
