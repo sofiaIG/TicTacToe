b6ad8959beece3710ad178da64b0e3f6e9d08ef0
sofiaIG <sphignatiadi@gmail.com>
2021-08-25 16:14:17 +0100

    For loop has been made to make the display_board shorter. Redundant is True has been removed.

--- main.py
+++ main.py
@@ -1,56 +1,61 @@
+"""
+This is a game of TicTacToe
+"""
+
 import random
 
-x = "X"
-o = "O"
-board = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+X = "X"
+O = "O"
 
 
-def display_board(board: list):
-    "This function in printing the board with its current values"
-    print("+-------+-------+-------+\n" "|       |       |       |\n"\
-          "|   {}   |   {}   |   {}   |\n"\
-          "|       |       |       |\n".format(board[0][0], board[0][1], board[0][2]), end ="")
-    print("+-------+-------+-------+\n" "|       |       |       |\n"
-          "|   {}   |   {}   |   {}   |\n"\
-          "|       |       |       |\n".format(board[1][0], board[1][1], board[1][2]), end ="")
-    print("+-------+-------+-------+\n" "|       |       |       |\n"\
-          "|   {}   |   {}   |   {}   |\n""|       |       |       |\n"\
-          "+-------+-------+-------+".format(board[2][0], board[2][1], board[2][2]), end ="")
+def display_board(board):
+    """
+    This function in printing the board with its current values
+    """
+    for i in range(0, 3):
+        print("+-------+-------+-------+")
+        print("|       |       |       |")
+        print("|   {}   |   {}   |   {}   |".format(board[i][0], board[i][1], board[i][2]))
+    print("+-------+-------+-------+")
 
 
 def remove_squares_with_x(board):
     "This is a function will create a board that only has squares that are not selected by X and O"
-    board_decision_removed = [[i for i in j if type(i)==int]for j in board]
+    board_decision_removed = [[i for i in j if isinstance(i, int)] for j in board]
     return board_decision_removed
 
-def enter_input():
+
+def enter_input(board):
     "This function is used to get players decision and place it on the board"
     board_user = remove_squares_with_x(board)
     flat_board_user = [i for sublist in board_user for i in sublist]
     users_choice = int(input("\n Please add a number from 0 to 8: "))
     while users_choice not in flat_board_user:
-        users_choice = int(input("\nPlease only enter numbers from 0-8 that has not been selected already):"))
-    adding_decision_into_board(users_choice, board, x)
+        users_choice = int(input(
+            "\nPlease only enter numbers from 0-8 that has not been selected already):"))
+    adding_decision_into_board(users_choice, board, X)
 
 
-def opponents_move():
-    "This function is used to get the user's move and add it into the board"
+def opponents_move(board):
+    """This function is used to get the user's move and add it into the board"""
     board_without_x_o = remove_squares_with_x(board)
-    if len(board_without_x_o)==0:
-        "You have finished the game"
+    if len(board_without_x_o) == 0:
+        print("You have finished the game")
     else:
-        flat = [x for sublist in board_without_x_o for x in sublist]
-        number_selected = random.choice(flat)
+        flat_board = [x for sublist in board_without_x_o for x in sublist]
+        number_selected = random.choice(flat_board)
         print("\nThe opponent has selected: ")
-        adding_decision_into_board(number_selected, board, o)
+        adding_decision_into_board(number_selected, board, O)
 
 
-def win_game(board,character):
-    "This function in used to decide who is winning. The functionality of it is limited to vertical and horizontal winning "
+def win_game(board, character):
+    """
+    This function in used to decide who is winning. The functionality
+    of it is limited to vertical and horizontal winning
+    """
     position_j = -1
     list_vertically = []
     list_diagonally = []
-    number = 0
     for i in board:
         if set(i) == {character}:
             print(f"{character} has won!")
@@ -69,11 +74,15 @@ def win_game(board,character):
 
 
 def winning_diagonally(board, character):
+    """
+    This function in used to decide who is winning. The functionality
+    of it is limited to diagonal winning
+    """
     number = 0
     list_for_1st_loop = []
     list_for_2st_loop = []
     for i in board:
-        for j in i:
+        for _ in i:
             if i[number] == character:
                 list_for_1st_loop.append(1)
                 if number <= 2:
@@ -85,7 +94,7 @@ def winning_diagonally(board, character):
     else:
         number_1 = 2
         for i in board:
-            for j in i:
+            for _ in i:
                 if i[number_1] == character:
                     list_for_2st_loop.append(1)
                 if number_1 >= 0:
@@ -97,9 +106,12 @@ def winning_diagonally(board, character):
 
 
 def adding_decision_into_board(value, board, character):
-    number = -1#Creating number as I can not use i for index so I am replacing it with number
+    """
+    Add a player's decision to the board and return the updated board
+    """
+    number = -1  # Creating number as I can not use i for index so I am replacing it with number
     for i in board:
-        number+=1
+        number += 1
         for j in i:
             index = i.index(j)
             if board[number][index] == value:
@@ -108,18 +120,19 @@ def adding_decision_into_board(value, board, character):
     return board
 
 
+the_board = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
 while True:
-    display_board(board)
-    enter_input()
-    if win_game(board,x) == True or winning_diagonally(board,x) == True:
-        display_board(board)
+    display_board(the_board)
+    enter_input(the_board)
+    if win_game(the_board, X) or winning_diagonally(the_board, X):
+        display_board(the_board)
         break
-    flat = [i for sublist in board for i in sublist]  # This is not working
-    if set(flat) == {x, o}:
+    flat = [i for sublist in the_board for i in sublist]
+    if set(flat) == {X, O}:
         print("The game is finished")
         break
-    opponents_move()
-    if win_game(board,o) == True or winning_diagonally(board, o) == True:
-        display_board(board)
+    opponents_move(the_board)
+    if win_game(the_board, O) or winning_diagonally(the_board, O):
+        display_board(the_board)
         break
-    win_game(board, o)
+    win_game(the_board, O)
